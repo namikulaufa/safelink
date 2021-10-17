8391b4359fa0d35a89f99a06e8015239549b45b8
namikulaufa <85009935+namikulaufa@users.noreply.github.com>
2021-10-17 16:32:49 +0700

    Update safe.js

--- safe.js
+++ safe.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://perpustakaan.sman3tamsel.sch.id/m/p/safe?url=" + encryptedUrl;
+a_to_ve[i].href = "https://perpustakaan.sman3tamsel.sch.id/m/p/safe/?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
